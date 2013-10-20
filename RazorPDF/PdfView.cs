74f8ccbbc00a71ba129c745ef7ee8f235f3c4312
Vincenzo Chianese <vincenz.chianese@yahoo.it>
2013-10-20 15:58:55 +0200

    Changes for new iTextSharp version

--- RazorPDF/PdfView.cs
+++ RazorPDF/PdfView.cs
@@ -1,40 +1,25 @@
-﻿// Copyright 2012 Al Nyveldt - http://nyveldt.com
-// 
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// 
-//     http://www.apache.org/licenses/LICENSE-2.0
-// 
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// 
+﻿using iTextSharp.text;
+using iTextSharp.text.html.simpleparser;
+using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Web;
 using System.Web.Mvc;
 using System.Xml;
-using System.Xml.Linq;
-using iTextSharp.text;
-using iTextSharp.text.html;
-using iTextSharp.text.pdf;
-using iTextSharp.text.xml;
 
 namespace RazorPDF
 {
     public class PdfView : IView, IViewEngine
     {
         private readonly ViewEngineResult _result;
-
         public PdfView(ViewEngineResult result)
         {
             _result = result;
         }
+
 
         public void Render(ViewContext viewContext, TextWriter writer)
         {
@@ -44,61 +29,55 @@ namespace RazorPDF
             _result.View.Render(viewContext, tw);
             var resultCache = sb.ToString();
 
-            // detect itext (or html) format of response
-            XmlParser parser;
-            using (var reader = GetXmlReader(resultCache))
-            {
-                while (reader.Read() && reader.NodeType != XmlNodeType.Element)
-                {
-                    // no-op
-                }
-
-                if (reader.NodeType == XmlNodeType.Element && reader.Name == "itext")
-                    parser = new XmlParser();
-                else
-                    parser = new HtmlParser();
-            }
-
-            // Create a document processing context
+            var ms = new MemoryStream();
             var document = new Document();
+            var pdfWriter = PdfWriter.GetInstance(document, ms);
+            var worker = new HTMLWorker(document);
             document.Open();
+            worker.StartDocument();
 
-            // associate output with response stream
-            var pdfWriter = PdfWriter.GetInstance(document, viewContext.HttpContext.Response.OutputStream);
             pdfWriter.CloseStream = false;
+
+
+            worker.Parse(new StringReader(resultCache));
+            worker.EndDocument();
+            worker.Close();
+            document.CloseDocument();
+            document.Close();
 
             // this is as close as we can get to being "success" before writing output
             // so set the content type now
             viewContext.HttpContext.Response.ContentType = "application/pdf";
-
-            // parse memory through document into output
-            using (var reader = GetXmlReader(resultCache))
-            {
-                parser.Go(document, reader);
-            }
-
+            pdfWriter.Flush();
             pdfWriter.Close();
+
+            viewContext.HttpContext.Response.BinaryWrite(ms.ToArray());
         }
+
 
         private static XmlTextReader GetXmlReader(string source)
         {
             byte[] byteArray = Encoding.UTF8.GetBytes(source);
             MemoryStream stream = new MemoryStream(byteArray);
 
+
             var xtr = new XmlTextReader(stream);
             xtr.WhitespaceHandling = WhitespaceHandling.None; // Helps iTextSharp parse 
             return xtr;
         }
+
 
         public ViewEngineResult FindPartialView(ControllerContext controllerContext, string partialViewName, bool useCache)
         {
             throw new System.NotImplementedException();
         }
 
+
         public ViewEngineResult FindView(ControllerContext controllerContext, string viewName, string masterName, bool useCache)
         {
             throw new System.NotImplementedException();
         }
+
 
         public void ReleaseView(ControllerContext controllerContext, IView view)
         {
